c2e841c73d036c7ba43b26d9c801bd455825470e
koshirok096 <koshirok096@gmail.com>
2022-11-24 13:28:14 -0800

    new

--- src/main/java/com/group1/movielist_app/controller/AuthController.java
+++ src/main/java/com/group1/movielist_app/controller/AuthController.java
@@ -11,8 +11,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.group1.movielist_app.dao.AuthRepository;
 import com.group1.movielist_app.entity.User;
 
+//Added
+// import java.util.List;
+// import com.group1.movielist_app.entity.MovieList;
+// import com.fasterxml.jackson.core.JsonProcessingException;
+// import java.util.Optional;
+// import com.group1.movielist_app.dao.UserRepository;
+
+
 @Controller
 public class AuthController {
+
+    // @Autowired
+    // UserRepository userRepositry;
 
     @Autowired
     AuthRepository authRepository;
@@ -22,12 +33,31 @@ public class AuthController {
         model.addAttribute("user", new User());
         return "auth/login";
     }
+    @GetMapping("/logout")
+    public String showLogout(Model model) {
+        model.addAttribute("user", new User());
+        return "auth/login";
+    }
 
     @GetMapping("/register")
     public String showRegister(Model model) {
         model.addAttribute("user", new User());
         return "auth/register";
     }
+
+    @GetMapping("/account")
+    public String showAccount(Model model) {
+        model.addAttribute("user", new User());
+        return "auth/account";
+    }
+
+    @GetMapping("/catalogue")
+    
+    public String showCatalogue(Model model) {
+        model.addAttribute("user", new User());
+        return "auth/catalogue";
+    }
+
 
     @PostMapping("/login")
     public String handleLogin(User user, Model model, HttpSession session) {
@@ -42,6 +72,12 @@ public class AuthController {
         }
     }
 
+    @PostMapping("/logout")
+    public String handleLogout(Model model, HttpSession session) {
+        session.removeAttribute(SessionConstants.APP_USERID);
+        return "redirect:/login";
+    }
+
     @PostMapping("/register")
     public String handleRegister(User user, Model model) {
         if (user.getFullName() != null && user.getEmail() != null && user.getPassword() != null) {
@@ -51,5 +87,26 @@ public class AuthController {
             return "redirect:/register";
         }
     }
+
+    @PostMapping("/account")
+    public String handleAccount(User user, Model model) {
+        if (user.getFullName() != null && user.getEmail() != null && user.getPassword() != null) {
+            authRepository.save(user);
+            return "redirect:/";
+        } else {
+            return "redirect:/account";
+        }
+    }
+
+    @PostMapping("/catalogue")
+    public String handleCatalogue(User user, Model model) {
+        if (user.getFullName() != null && user.getEmail() != null && user.getPassword() != null) {
+            authRepository.save(user);
+            return "redirect:/";
+        } else {
+            return "redirect:/catalogue";
+        }
+    }
+
 
 }
